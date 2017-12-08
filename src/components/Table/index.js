7f0b5d31e178722802656ffa9406ba926338185f
Jeremi Anastaziak <jeremi.anastaziak@allegrogroup.com>
2017-12-08 23:46:02 +0100

    responsive / route

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -13,30 +13,40 @@ import { Card } from 'material-ui/Card';
 import CircularProgress from 'material-ui/CircularProgress';
 import TableToolbar from '../TableToolbar';
 import TableRowCustom from '../TableRow';
+import './hack.css';
 
 const TableCustom = ({ records, isFetching, alarmTable }) => {
     return (
-        <Card className="row-margins">
-            <TableToolbar isAlarmTable={alarmTable}/>
-            <Table>
-                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                    <TableRow>
-                        <TableHeaderColumn>Date</TableHeaderColumn>
-                        <TableHeaderColumn>Temperatura</TableHeaderColumn>
-                        <TableHeaderColumn>Poziom dzwieku</TableHeaderColumn>
-                        <TableHeaderColumn>Stan klamki</TableHeaderColumn>
-                        <TableHeaderColumn>Alarmy</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
+        <Card
+            className="row-margins"
+            style={{ position: 'relative', overflow: 'hidden' }}
+        >
+            <TableToolbar isAlarmTable={alarmTable} />
+            <div style={{ overflowX: 'scroll', overflowY: 'hidden' }}>
+                <Table style={{ minWidth: '700px' }}>
+                    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                        <TableRow>
+                            <TableHeaderColumn>Date</TableHeaderColumn>
+                            <TableHeaderColumn>Temperatura</TableHeaderColumn>
+                            <TableHeaderColumn>Poziom dzwieku</TableHeaderColumn>
+                            <TableHeaderColumn>Stan klamki</TableHeaderColumn>
+                            <TableHeaderColumn>Alarmy</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
 
-                <TableBody displayRowCheckbox={false}>
-                    {isFetching && <CircularProgress />}
-                    {records &&
-                        records.map(record => (
-                            <TableRowCustom key={uuidv1()} record={record} isFetching={isFetching}/>
-                        ))}
-                </TableBody>
-            </Table>
+                    <TableBody displayRowCheckbox={false}>
+                        {isFetching && <CircularProgress />}
+                        {records &&
+                            records.map(record => (
+                                <TableRowCustom
+                                    key={uuidv1()}
+                                    record={record}
+                                    isFetching={isFetching}
+                                />
+                            ))}
+                    </TableBody>
+                </Table>
+            </div>
         </Card>
     );
 };
